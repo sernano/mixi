0a7affa429c495153d047e4dc3bbdffea08b6e63
Samantha Ernano <samantha.ernano@gmail.com>
2020-11-19 17:05:14 -0500

    Initialized Amplitude in player component

--- client/components/Player.js
+++ client/components/Player.js
@@ -1,14 +1,51 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import Amplitude from 'amplitudejs';
+import {fetchAllSongs} from '../store/songs';
 
-export default class Player extends React.Component {
-  componentDidMount() {}
+export class Player extends React.Component {
+  async componentDidMount() {
+    await this.props.fetchAllSongs();
+    const songs = this.formatSongs(this.props.songs);
+    Amplitude.init({songs: songs});
+  }
+
   render() {
     return (
       <div>
-        <button type="button">Play</button>
-        <button type="button">Stop</button>
+        <button type="button" onClick={Amplitude.play}>
+          Play
+        </button>
+        <button type="button" onClick={Amplitude.stop}>
+          Stop
+        </button>
       </div>
     );
   }
+
+  formatSongs(songs) {
+    return songs.map(song => {
+      return {
+        name: song.name,
+        artist: song.artist,
+        album: song.album,
+        url: song.songUrl,
+        cover_art_url: song.coverArtUrl
+      };
+    });
+  }
 }
+
+const mapState = state => {
+  return {
+    songs: state.songs
+  };
+};
+
+const mapDispatch = dispatch => {
+  return {
+    fetchAllSongs: () => dispatch(fetchAllSongs())
+  };
+};
+
+export default connect(mapState, mapDispatch)(Player);
