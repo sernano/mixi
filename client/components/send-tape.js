32d4fb283910562056f2995679184f3d52a053a2
Samantha Ernano <samantha.ernano@gmail.com>
2020-11-20 17:00:52 -0500

    Moved uppy to class

--- client/components/send-tape.js
+++ client/components/send-tape.js
@@ -2,36 +2,47 @@ import React from 'react';
 import Uppy from '@uppy/core';
 import ThumbnailGenerator from '@uppy/thumbnail-generator';
 import XHRUpload from '@uppy/xhr-upload';
-import Tus from '@uppy/tus';
 
 import {DragDrop} from '@uppy/react';
-
-const uppy = new Uppy({
-  restrictions: {maxNumberOfFiles: 20, maxFileSize: 10485760},
-  allowedFileTypes: ['.mp3', ',m4a'],
-  autoProceed: false
-});
-
-uppy.use(XHRUpload, {
-  endpoint: '/api/songs/upload',
-  method: 'post',
-  formData: true
-});
 
 class SendTape extends React.Component {
   constructor(props) {
     super(props);
+    this.uppy = new Uppy({
+      restrictions: {maxNumberOfFiles: 20, maxFileSize: 10485760},
+      allowedFileTypes: ['.mp3', ',m4a'],
+      autoProceed: false
+    });
+
+    this.state = {
+      filesToUpload: []
+    };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    this.uppy.use(XHRUpload, {
+      endpoint: '/api/songs/upload',
+      method: 'post',
+      formData: true
+    });
+    this.uppy.on('file-added', file => {
+      console.log('Added file', file);
+      const fileAdded = this.uppy.getFiles();
+      this.setState({
+        filesToUpload: fileAdded
+      });
+      console.log('state:', this.state);
+    });
+  }
   render() {
+    console.log('state:', this.state);
     return (
       <>
         <DragDrop
           width="70%"
           height="70%"
           note="Drop yer songs here"
-          uppy={uppy}
+          uppy={this.uppy}
           locale={{
             strings: {
               // Text to show on the droppable area.
@@ -44,7 +55,7 @@ class SendTape extends React.Component {
         />
         <br />
         <div>
-          <button type="button" onClick={uppy.upload}>
+          <button type="button" onClick={this.uppy.upload}>
             Upload
           </button>
         </div>
