731dde4a1fc3390bda51051c969718e51cce192a
Samantha Ernano <samantha.ernano@gmail.com>
2021-02-01 18:25:48 -0500

    Implemented React Forms on the auth component

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -2,6 +2,15 @@ import React from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import {auth} from '../store';
+import {
+  Row,
+  Col,
+  Form,
+  FormControl,
+  FormGroup,
+  FormLabel,
+  Button
+} from 'react-bootstrap';
 
 /**
  * COMPONENT
@@ -10,34 +19,33 @@ const AuthForm = props => {
   const {name, displayName, handleSubmit, error} = props;
 
   return (
-    <div className="login-signup">
-      <h2>{displayName}</h2>
-      <form className="login-signup-form" onSubmit={handleSubmit} name={name}>
-        <div>
-          <input name="email" type="text" placeholder="email" required />
-        </div>
-        <div>
-          <input
-            name="password"
-            type="password"
-            placeholder="password"
-            required
-          />
-        </div>
-        <div>
-          <button type="submit" className="button">
-            {displayName}
-          </button>
-        </div>
-        {error &&
-          error.response && (
-            <div>
-              {' '}
-              <h4>{error.response.data}</h4>{' '}
-            </div>
-          )}
-      </form>
-    </div>
+    <Row className="login-signup">
+      <Col>
+        <h2>{displayName}</h2>
+        <Form onSubmit={handleSubmit} name={name} className="login-form">
+          <FormGroup controlId="login">
+            <FormLabel>Email address</FormLabel>
+            <FormControl type="email" placeholder="Enter email" required />
+          </FormGroup>
+          <FormGroup>
+            <FormLabel>Password</FormLabel>
+            <FormControl type="password" placeholder="Password" required />
+          </FormGroup>
+          <div>
+            <Button variant="primary" type="submit">
+              {displayName}
+            </Button>
+          </div>
+          {error &&
+            error.response && (
+              <div>
+                {' '}
+                <h4>{error.response.data}</h4>{' '}
+              </div>
+            )}
+        </Form>
+      </Col>
+    </Row>
   );
 };
 
