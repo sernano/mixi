d060af5dbfc11aaa8e25440c397b49e7b708c8c1
Samantha Ernano <samantha.ernano@gmail.com>
2021-02-02 12:17:56 -0500

    Styled nav bar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,43 +3,46 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {logout} from '../store';
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container, Row, Col, Nav} from 'react-bootstrap';
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <Container>
     <Row>
-      <Link to="/home">
-        <h1>Mixi</h1>
-      </Link>
-      <nav>
+      <Col md={8}>
+        <Link to="/home">
+          <h1>Mixi</h1>
+        </Link>
+      </Col>
+      <Col className="justify-content-end">
         {isLoggedIn ? loggedInNavbar(handleClick) : loggedOutNavbar()}
-        <div />
-      </nav>
+      </Col>
     </Row>
   </Container>
 );
 
 function loggedInNavbar(handleClick) {
   return (
-    <Col>
-      {/* The navbar will show these links after you log in */}
-      <Link to="/my-tapes">My Tapes</Link>
-      <Link to="/upload-songs">Upload Songs</Link>
-      <a href="#" onClick={handleClick}>
-        Logout
-      </a>
-    </Col>
+    <>
+      <Nav className="justify-content-end" id="nav-links">
+        {/* The navbar will show these links after you log in */}
+        <Link to="/my-tapes">My Tapes</Link>
+        <Link to="/upload-songs">Upload Songs</Link>
+        <a href="#" onClick={handleClick}>
+          Logout
+        </a>
+      </Nav>
+    </>
   );
 }
 
 function loggedOutNavbar() {
   return (
     <>
-      <Col>
+      <Nav className="justify-content-end" id="nav-links">
         {/* The navbar will show these links before you log in */}
         <Link to="/login">Login</Link>
         <Link to="/signup">Sign Up</Link>
-      </Col>
+      </Nav>
     </>
   );
 }
