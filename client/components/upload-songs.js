fe337aded99d0d76263eb109158ead3c5a3383fb
Samantha Ernano <samantha.ernano@gmail.com>
2021-02-04 18:41:41 -0500

    Bootstrapped upload songs page

--- client/components/upload-songs.js
+++ client/components/upload-songs.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Uppy from '@uppy/core';
 import XHRUpload from '@uppy/xhr-upload';
-
 import {DragDrop} from '@uppy/react';
+import {Row, Col, Button} from 'react-bootstrap';
 
 class UploadSongs extends React.Component {
   constructor(props) {
@@ -42,43 +42,43 @@ class UploadSongs extends React.Component {
 
   render() {
     return (
-      <>
-        <h2>Upload Songs</h2>
-        <div className="split-container">
-          <div id="uppy-component">
-            <DragDrop
-              width="100%"
-              height="100%"
-              note=".mp3s only, plz"
-              uppy={this.uppy}
-              locale={{
-                strings: {
-                  // Text to show on the droppable area.
-                  // `%{browse}` is replaced with a link that opens the system file selection dialog.
-                  dropHereOr: 'Drop here or %{browse}',
-                  // Used as the label for the link that opens the system file selection dialog.
-                  browse: 'browse'
-                }
-              }}
-            />
-          </div>
-          <div id="songs-to-upload">
-            <h3>Files selected:</h3>
-            {this.state.filesToUpload.map(file => {
-              return (
-                <div key={file.size}>
-                  <h4>{file.name}</h4>
-                </div>
-              );
-            })}
-          </div>
-        </div>
+      <Row className="justify-content-center">
+        <Col xs={12}>
+          <h2>Upload Songs</h2>
+        </Col>
+        <Col md={6}>
+          <DragDrop
+            width="100%"
+            height="100%"
+            note=".mp3s only, plz"
+            uppy={this.uppy}
+            locale={{
+              strings: {
+                // Text to show on the droppable area.
+                // `%{browse}` is replaced with a link that opens the system file selection dialog.
+                dropHereOr: 'Drop here or %{browse}',
+                // Used as the label for the link that opens the system file selection dialog.
+                browse: 'browse'
+              }
+            }}
+          />
+        </Col>
+        <Col md={6}>
+          <h3>Files selected:</h3>
+          {this.state.filesToUpload.map(file => {
+            return (
+              <div key={file.size}>
+                <h4>{file.name}</h4>
+              </div>
+            );
+          })}
+        </Col>
         <div>
-          <button type="button" className="button" onClick={this.handleUpload}>
+          <Button variant="primary" onClick={this.handleUpload}>
             Upload
-          </button>
+          </Button>
         </div>
-      </>
+      </Row>
     );
   }
 }
