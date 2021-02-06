b9561efffa28d57b09a6650b744afab42e202ee9
Samantha Ernano <samantha.ernano@gmail.com>
2021-02-05 22:25:44 -0500

    Fix blank background gradient when page is scrollable.

--- client/app.js
+++ client/app.js
@@ -6,11 +6,12 @@ import {Container} from 'react-bootstrap';
 
 const App = () => {
   return (
-    <div className="h-100 wrapper d-flex flex-column">
+    <div className="h-100 d-flex flex-column">
       <Navbar />
       <Container className="mt-5 mb-1 flex-grow-1">
         <Routes />
       </Container>
+      <div className="underlay" />
     </div>
   );
 };
