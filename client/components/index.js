0b853e00455011c365ad6fc54b5501df3dee689a
Samantha Ernano <samantha.ernano@gmail.com>
2021-02-16 17:33:45 -0500

    Removed make-tape component

--- client/components/index.js
+++ client/components/index.js
@@ -7,7 +7,6 @@ export {default as Navbar} from './navbar';
 export {default as UserHome} from './user-home';
 export {default as Player} from './player';
 export {default as UploadSongs} from './upload-songs';
-export {default as MakeTape} from './make-tape';
 export {default as MyTapes} from './my-tapes';
 export {default as EditTape} from './edit-tape';
 export {Login, Signup} from './auth-form';
