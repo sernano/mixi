fe84774be2e5362571999a6d6a8155d285e71b86
Samantha Ernano <samantha.ernano@gmail.com>
2021-02-16 12:50:00 -0500

    Formatted spacing and styles on My Tapes component

--- client/components/my-tapes.js
+++ client/components/my-tapes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {fetchTapes} from '../store/tapes';
-import {Col, Image} from 'react-bootstrap';
+import {Row, Col, Image} from 'react-bootstrap';
 
 class MyTapes extends React.Component {
   componentDidMount() {
@@ -10,20 +10,22 @@ class MyTapes extends React.Component {
   }
   render() {
     return (
-      <Col className="align-items-md-start align-items-center col d-flex flex-column">
-        <h2 className="mb-4">My Tapes</h2>
-        {this.props.tapes.map(tape => {
-          return (
-            <div key={tape.id}>
-              <Image
-                src={tape.albumArtUrl}
-                onClick={() => this.handleClick(tape.id)}
-                className="img-thumbnail album-art"
-              />
-              <h6 className="my-0">{tape.title}</h6>
-            </div>
-          );
-        })}
+      <Col>
+        <h2 className="mb-4 text-center text-md-left">My Tapes</h2>
+        <Row>
+          {this.props.tapes.map(tape => {
+            return (
+              <Col key={tape.id} md={3} sm={6} className="text-center">
+                <Image
+                  src={tape.albumArtUrl}
+                  onClick={() => this.handleClick(tape.id)}
+                  className="img-thumbnail album-art"
+                />
+                <h6 className="mt-2">{tape.title}</h6>
+              </Col>
+            );
+          })}
+        </Row>
         <Link to="/make-tape">
           <h6 className="text-center">Make a new tape</h6>
         </Link>
