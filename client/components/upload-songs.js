43cd488c4a6983133959b3cb28bc5dc7c4ae2233
Samantha Ernano <samantha.ernano@gmail.com>
2021-02-21 19:56:52 -0500

    Turned on auto proceed for uploads

--- client/components/upload-songs.js
+++ client/components/upload-songs.js
@@ -3,7 +3,7 @@ import {useHistory} from 'react-router-dom';
 import Uppy from '@uppy/core';
 import XHRUpload from '@uppy/xhr-upload';
 import {DragDrop} from '@uppy/react';
-import {Modal, Button} from 'react-bootstrap';
+import {Modal} from 'react-bootstrap';
 
 export default function UploadSongs(props) {
   const history = useHistory();
@@ -12,7 +12,7 @@ export default function UploadSongs(props) {
   const uppy = new Uppy({
     restrictions: {maxNumberOfFiles: 20},
     allowedFileTypes: ['.mp3'],
-    autoProceed: false
+    autoProceed: true
   });
 
   useEffect(() => {
@@ -26,11 +26,12 @@ export default function UploadSongs(props) {
     };
   });
 
-  const handleUpload = async () => {
-    await uppy.upload();
+  const onUploadComplete = () => {
     history.push('/my-tapes');
     props.closeUpload();
   };
+
+  uppy.on('complete', onUploadComplete);
 
   return (
     <Modal show={props.showUpload} onHide={props.closeUpload}>
@@ -42,25 +43,18 @@ export default function UploadSongs(props) {
           className="justify-content-center"
           width="100%"
           height="100%"
-          note=".mp3s only, plz"
+          note=".mp3s only"
           uppy={uppy}
           locale={{
             strings: {
               // Text to show on the droppable area.
               // `%{browse}` is replaced with a link that opens the system file selection dialog.
-              dropHereOr: 'Drop here or %{browse}',
+              dropHereOr: 'Drop here or click to %{browse}',
               // Used as the label for the link that opens the system file selection dialog.
               browse: 'browse'
             }
           }}
         />
-        <Button
-          variant="primary"
-          onClick={handleUpload}
-          className="btn-block mt-4"
-        >
-          Upload
-        </Button>
       </Modal.Body>
     </Modal>
   );
