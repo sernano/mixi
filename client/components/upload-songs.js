1fc8f4b8585f198842ab6957c8483ee2fb01250b
Samantha Ernano <samantha.ernano@gmail.com>
2021-02-21 19:17:22 -0500

    Refactored upload songs to be a functional component

--- client/components/upload-songs.js
+++ client/components/upload-songs.js
@@ -1,71 +1,67 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useHistory} from 'react-router-dom';
 import Uppy from '@uppy/core';
 import XHRUpload from '@uppy/xhr-upload';
 import {DragDrop} from '@uppy/react';
 import {Modal, Button} from 'react-bootstrap';
 
-class UploadSongs extends React.Component {
-  constructor(props) {
-    super(props);
-    this.uppy = new Uppy({
-      restrictions: {maxNumberOfFiles: 20},
-      allowedFileTypes: ['.mp3'],
-      autoProceed: false
-    });
-    this.handleUpload = this.handleUpload.bind(this);
-  }
+export default function UploadSongs(props) {
+  const history = useHistory();
 
-  async handleUpload() {
-    await this.uppy.upload();
-    this.props.closeUpload();
-  }
+  // Initialize Uppy
+  const uppy = new Uppy({
+    restrictions: {maxNumberOfFiles: 20},
+    allowedFileTypes: ['.mp3'],
+    autoProceed: false
+  });
 
-  componentDidMount() {
-    this.uppy.use(XHRUpload, {
+  useEffect(() => {
+    uppy.use(XHRUpload, {
       endpoint: '/api/songs/',
       method: 'post',
       formData: true
     });
-  }
+    return () => {
+      uppy.close();
+    };
+  });
 
-  componentWillUnmount() {
-    this.uppy.close();
-  }
+  const handleUpload = async () => {
+    await uppy.upload();
+    history.push('/my-tapes');
+    props.closeUpload();
+  };
 
-  render() {
-    return (
-      <Modal show={this.props.showUpload} onHide={this.props.closeUpload}>
-        <Modal.Header closeButton>
-          <Modal.Title>Upload Songs</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <DragDrop
-            className="justify-content-center"
-            width="100%"
-            height="100%"
-            note=".mp3s only, plz"
-            uppy={this.uppy}
-            locale={{
-              strings: {
-                // Text to show on the droppable area.
-                // `%{browse}` is replaced with a link that opens the system file selection dialog.
-                dropHereOr: 'Drop here or %{browse}',
-                // Used as the label for the link that opens the system file selection dialog.
-                browse: 'browse'
-              }
-            }}
-          />
-          <Button
-            variant="primary"
-            onClick={this.handleUpload}
-            className="btn-block mt-4"
-          >
-            Upload
-          </Button>
-        </Modal.Body>
-      </Modal>
-    );
-  }
+  return (
+    <Modal show={props.showUpload} onHide={props.closeUpload}>
+      <Modal.Header closeButton>
+        <Modal.Title>Upload Songs</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <DragDrop
+          className="justify-content-center"
+          width="100%"
+          height="100%"
+          note=".mp3s only, plz"
+          uppy={uppy}
+          locale={{
+            strings: {
+              // Text to show on the droppable area.
+              // `%{browse}` is replaced with a link that opens the system file selection dialog.
+              dropHereOr: 'Drop here or %{browse}',
+              // Used as the label for the link that opens the system file selection dialog.
+              browse: 'browse'
+            }
+          }}
+        />
+        <Button
+          variant="primary"
+          onClick={handleUpload}
+          className="btn-block mt-4"
+        >
+          Upload
+        </Button>
+      </Modal.Body>
+    </Modal>
+  );
 }
-
-export default UploadSongs;
